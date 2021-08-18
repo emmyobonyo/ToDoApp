8b1f4d470570177efc75f9d0abff6b8da2620292
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-08-18 20:38:40 +0300

    Maintain list checkbox and paragraph status on refresh

--- src/render.js
+++ src/render.js
@@ -1,33 +1,35 @@
 const ul = document.getElementById('list-items');
 const button = document.getElementById('submit');
 
-const tasks = [
-  {
-    index: 0,
-    isCompleted: false,
-    description: 'get some excercise',
-  },
-  {
-    index: 1,
-    isCompleted: false,
-    description: 'Read the Bible',
-  },
-  {
-    index: 2,
-    isCompleted: false,
-    description: 'Shower',
-  },
-  {
-    index: 3,
-    isCompleted: false,
-    description: 'Take breakfast',
-  },
-];
-
-localStorage.setItem('tasks', JSON.stringify(tasks));
-const getTasks = JSON.parse(localStorage.getItem('tasks'));
-
 const rendered = () => {
+  const tasks = [
+    {
+      index: 0,
+      isCompleted: false,
+      description: 'get some excercise',
+    },
+    {
+      index: 1,
+      isCompleted: false,
+      description: 'Read the Bible',
+    },
+    {
+      index: 2,
+      isCompleted: false,
+      description: 'Shower',
+    },
+    {
+      index: 3,
+      isCompleted: false,
+      description: 'Take breakfast',
+    },
+  ];
+
+  if (localStorage.length == 0) {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }
+  const getTasks = JSON.parse(localStorage.getItem('tasks'));
+
   for (let i = 0; i < getTasks.length; i += 1) {
     const li = document.createElement('li');
     li.className = 'list-class';
@@ -38,21 +40,23 @@ const rendered = () => {
     checkbox.onclick = () => {
       if (checkbox.checked === true) {
         p.style.textDecoration = 'line-through';
-        tasks[i].isCompleted = true;
+        getTasks[i].isCompleted = true;
         li.className = 'delete';
-        getTasks.splice(i, 1);
         localStorage.setItem('tasks', JSON.stringify(getTasks));
       } else {
         p.style.textDecoration = 'none';
-        tasks[i].isCompleted = false;
+        getTasks[i].isCompleted = false;
         li.className = 'undelete';
-        getTasks.splice(i, 1);
         localStorage.setItem('tasks', JSON.stringify(getTasks));
       }
     };
     const icon = document.createElement('i');
     icon.className = 'fa fa-bars';
     p.innerHTML = getTasks[i].description;
+    if (getTasks[i].isCompleted){
+      p.style.textDecoration = 'line-through';
+      checkbox.checked = true;
+    }
     ul.appendChild(li);
     li.appendChild(liDiv);
     liDiv.appendChild(checkbox);
@@ -63,8 +67,11 @@ const rendered = () => {
 
 const sort = () => {
   const hide = document.getElementsByClassName('delete');
+  const getTasks = JSON.parse(localStorage.getItem('tasks'));
   for (let i = 0; i < hide.length; i += 1) {
     hide[i].style.display = 'none';
+    getTasks.splice(i, 1);
+    localStorage.setItem('tasks', JSON.stringify(getTasks));
   }
 };
 
